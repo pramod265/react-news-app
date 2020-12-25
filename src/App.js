ca7abb1e053f32e40cc60ddd5886e77099744e9f
Pramod Gupta <pga286478@gmail.com>
2020-12-26 02:39:03 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react'
+// import axios from 'axios'
+import Loading from "./Components/Loading";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [items, setItems] = useState( [] );
+  const [searchQuery, setSearchQuery] = useState('');
+  const [topiCondition, setTopiCondition] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search');
+
+  const fetchNews = () => {
+    setLoading(true);
+    fetch(url)
+    .then(result => result.json())
+    .then(data => {
+      console.log(data)
+      setItems(data.hits);
+      setLoading(false);
+    })
+    .catch(error => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchNews();
+  }, [url]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+    setTopiCondition(true);
+  }
+  
+  return(
+    <div>
+      <h2 style={{color:'blue'}}> Science & Tech News </h2>
+
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Search Your Topic' value={searchQuery} onChange={handleChange} />
+        <button >Search</button>
+      </form>
+
+      <div>
+        { !loading ? ( <div>
+            { topiCondition ? (<div> <h3> Topic - {searchQuery} </h3> </div>) : '' }
+            {items.map((n, i) => (
+                <p key={i}> {n.title} </p>
+            ))};
+            </div>
+            ): (<Loading message="Waiting for data..... " message_color='red' />)
+        }
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/Loading.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Loading = ({message, message_color}) => ( <h2 style={{color:message_color}}> {message} </h2>)
+
+
+export default Loading;
+
